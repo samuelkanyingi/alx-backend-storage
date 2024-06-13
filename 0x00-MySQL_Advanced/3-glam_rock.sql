9768395abeef0b4d37a5db1d2e6e77884ef1ec0d
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-06-13 15:07:23 +0000

    glamrock band

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,13 @@
+-- glam rock band
+SELECT 
+    band_name, 
+    CASE 
+        WHEN split IS NULL THEN 2022 - formed 
+        ELSE split - formed 
+    END AS lifespan
+FROM 
+    metal_bands
+WHERE 
+    main_style = 'Glam rock'
+ORDER BY 
+    lifespan DESC;
