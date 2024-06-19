93ef2f7eaebc6910eb3c61d103c156ab00b7050f
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-06-19 12:43:28 +0000

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,14 +4,27 @@ Cache module
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
+import functools
+
+
+def count_calls(method: Callable) -> Callable:
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        # Increment the counter for this method
+        key = method.__qualname__
+        self._redis.incr(key)
+        # Call the original method
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
-
+    
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
@@ -42,3 +55,10 @@ if __name__ == "__main__":
 
     local_redis = redis.Redis()
     print(local_redis.get(key))
+
+    cache.store(b"first")
+    print(cache.get(cache.store.__qualname__))
+
+    cache.store(b"second")
+    cache.store(b"third")
+    print(cache.get(cache.store.__qualname__))
